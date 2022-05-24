f6d14871d56878d21aef8a2de0d8f6d778319410
bun913 <imanau03@gmail.com>
2022-05-24 16:30:51 +0900

    feat: weztermの追加

---
+++ .wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm';
+
+return {
+  keys = {
+    -- Turn off the default CMD-m Hide action, allowing CMD-m to
+    -- be potentially recognized and handled by the tab
+    {
+      key="m",
+      mods="CMD",
+      action="DisableDefaultAssignment"
+    }
+  },
+  font_size = 18.0,
+  color_scheme = "GitHub Dark",
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  window_background_opacity = 0.8,
+}
