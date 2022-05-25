de573034cb2de181c6fe9fc5ff68afe6108a658b
bun913 <imanau03@gmail.com>
2022-05-26 08:25:22 +0900

    chore: add abc210 ploblems

---
+++ abc210/a/main.py
@@ -0,0 +1,21 @@
+"""
+Atcoderの問題解く用
+
+1行1列データ
+
+#str型で受け取るとき
+s = input() 
+#int型で受け取るとき
+s = int(input()) 
+#float型　(小数)で受け取るとき
+s = float(input())
+
+(1,N)行列データ
+s = input().split()
+# listで整数で受け取る
+l = list(map(int, input().split()))
+
+その他
+https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+"""
+
