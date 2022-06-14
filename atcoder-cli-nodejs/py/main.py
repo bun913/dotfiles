3cc76aabc929d916321c4d3ac066c552e7496cf2
bun913 <imanau03@gmail.com>
2022-06-14 15:53:10 +0900

    chore: add 'bit full search hint' on atcoder python template

--- atcoder-cli-nodejs/py/main.py
+++ atcoder-cli-nodejs/py/main.py
@@ -22,6 +22,9 @@ https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 全ての組み合わせを列挙する方法
 list(0...8)から2つを抜き出す
 list(combinations(l, 2))
+
+bit全探索でフラグが立っているかチェックする
+if ((i >> j) & 1)
 """
 from functools import reduce
 from itertools import combinations
