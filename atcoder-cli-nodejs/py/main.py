6d8e550e080622498f7af0685aa8af55a941ecf4
bun913 <imanau03@gmail.com>
2022-07-02 21:34:06 +0900

    feat: add lru_cache to atcoder python template

--- atcoder-cli-nodejs/py/main.py
+++ atcoder-cli-nodejs/py/main.py
@@ -9,6 +9,6 @@ list(combinations(l, 2))
 bit全探索でフラグが立っているかチェックする
 if ((i >> j) & 1)
 """
-from functools import reduce
+from functools import reduce, lru_cache
 from itertools import combinations
 import math
