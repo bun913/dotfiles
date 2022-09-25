57499abd85704ddf5add242089c0d5074e3ea3a6
bun913 <imanau03@gmail.com>
2022-09-25 14:41:48 +0900

    chore: update atcoder python template

--- atcoder-cli-nodejs/py/main.py
+++ atcoder-cli-nodejs/py/main.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Atcoderの問題解く用
-
-全ての組み合わせを列挙する方法
-list(0...8)から2つを抜き出す
-list(combinations(l, 2))
-
-bit全探索でフラグが立っているかチェックする
-if ((i >> j) & 1)
+解く前のメモ用
 """
-from functools import reduce, lru_cache
-from itertools import combinations
-import math
+from sys import setrecursionlimit
+
+setrecursionlimit(10**7)
