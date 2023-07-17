7a2a3434b2dd7521e373bc05912bd932101be3a1
bun913 <imanau03@gmail.com>
2023-07-17 21:01:09 +0900

    feat: live_grepをctrl+mに変更

--- config/nvim/lua/telescope_setting.lua
+++ config/nvim/lua/telescope_setting.lua
@@ -20,7 +20,7 @@ local actions = require('telescope.actions')
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<C-p>", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "<C-g>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<C-m>", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<M-p>", "<cmd>lua require('telescope.builtin').find_files( { cwd = vim.fn.expand('%:p:h') })<cr>", opts)
 keymap("n", "<M-g>", "<cmd>lua require('telescope.builtin').live_grep( { cwd = vim.fn.expand('%:p:h') })<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
